f8a5322f248627feb50bd9e29bb073390f32a6d9
Péter Kovács <peter.kovacs0519@gmail.com>
2018-11-25 16:09:08 +0100

    LFLT-307 Migrate common Leaflet components to OpenJDK 11  - Added module-info.java  - Fixed issues related to introduction of module descriptor

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module leaflet.mailer.component {
+    requires java.annotation;
+    requires java.compiler;
+    requires java.mail;
+    requires java.validation;
+    requires io.reactivex.rxjava2;
+    requires org.apache.commons.lang3;
+    requires slf4j.api;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.context.support;
+    requires spring.core;
+    requires thymeleaf;
+    requires thymeleaf.spring4;
+}
